aa3dccf28b152d90d19f0c97976d59fe1d6c91b9
gitrust <weebac@web.de>
2017-12-28 23:24:01 +0100

    fix paths

--- toIpod2.bat
+++ toIpod2.bat
@@ -1,2 +1,3 @@
-@echo on
-h:\Applications\PortableApps\Floola\Floola\ffmpeg -i %1 -f mp4 -r 25 -s 320×240 -b 768000 -ac 2 -ar 44000 -vol 400 -ab 112000 c:\temp\%2.mp4
+@echo off
+
+ffmpeg -i %1 -f mp4 -r 25 -s 320×240 -b 768000 -ac 2 -ar 44000 -vol 400 -ab 112000 %2.mp4
