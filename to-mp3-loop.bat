cddbd2903e94c1f6f3d0b1bccdfd7dbd1e5d97b1
gitrust <weebac@web.de>
2023-01-03 14:30:44 +0100

    Reduce input parameters

--- to-mp3-loop.bat
+++ to-mp3-loop.bat
@@ -2,9 +2,11 @@
 
 :: convert each audio in given directory to mp3
 
+setlocal
 set dir1=.
 
 :: extension of source audio
 set ext=*.wma
 
 FOR %%n in (%dir1% %ext%) DO  ffmpeg -i "%%n" -ab 128k  "new\%%~nn.mp3" 
+endlocal
--- to-mp3.bat
+++ to-mp3.bat
@@ -2,9 +2,10 @@
 
 :: Convert given audio to mp3
 
+setlocal
 set INPUT=%1
-set OUTPUT=%2
 set BITRATE=192
 
 :: avi to mp3
-ffmpeg -i "%INPUT%" -vn -ar 44100 -ac 2 -ab %BITRATE% -f mp3 "%OUTPUT%.mp3"
+ffmpeg -i "%INPUT%" -vn -ar 44100 -ac 2 -ab %BITRATE% -f mp3 "%INPUT%.mp3"
+endlocal
