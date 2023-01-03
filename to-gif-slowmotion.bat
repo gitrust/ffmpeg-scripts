929dcd775ea3501896d8937b520349794d7688c1
gitrust <weebac@web.de>
2023-01-03 13:49:41 +0100

    Rework to-gif-slowmotion

--- to-gif-slowmotion.bat
+++ to-gif-slowmotion.bat
@@ -3,13 +3,21 @@
 ::
 :: Create high quality gif from image sequence
 ::
-:: image format:   image-001.jpg
+:: image format:   img-001.jpg
+:: 
+
+setlocal
+set INPUT=img-%%03d.jpg
+set OUTPUT=video.gif
+set TMP_VIDEO=video.mp4
+set PALETTE=palette.png
 
 :: first, create palette
-ffmpeg -f image2 -i img-%%03d.jpg -vf scale=400:-1:sws_dither=ed,palettegen palette.png
+ffmpeg -f image2 -i "%INPUT%" -vf scale=400:-1:sws_dither=ed,palettegen %PALETTE%
 
 :: intermediate video
-ffmpeg.exe -f image2 -framerate 5 -i img-%%03d.jpg video.flv
+ffmpeg -f image2 -framerate 5 -i "%INPUT%" %TMP_VIDEO%
 
 :: final result gif
-ffmpeg.exe -i video.flv -framerate 5 -i palette.png -filter_complex "scale=400:-1:flags=lanczos[x];[x][1:v]paletteuse" video.gif
+ffmpeg -i %TMP_VIDEO% -framerate 5 -i %PALETTE% -filter_complex "scale=400:-1:flags=lanczos[x];[x][1:v]paletteuse" %OUTPUT%
+endlocal
