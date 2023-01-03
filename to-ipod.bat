b432b2e89804b44bbf7379e1a124d28114fe33f2
gitrust <weebac@web.de>
2023-01-03 13:50:07 +0100

    Reduce input to one parameter

--- to-ipod.bat
+++ to-ipod.bat
@@ -1,9 +1,11 @@
 @echo off
 
-:: Converts video to IPOD compatible format
+:: Converts video to iPOD compatible format
 
+setlocal
 set INPUT=%1
-set OUTPUT=%2
 set TITLE="video title"
 
-ffmpeg -i "%INPUT%" -acodec libfaac -ab 128k -s 320x240 -vcodec libx264 -vpre hq -vpre ipod320 -b 700k -bt 700k -metadata "title=%TITLE%" -threads 0 -f ipod "%OUTPUT%.mp4"
+ffmpeg -i "%INPUT%" -acodec libfaac -ab 128k -s 320x240 -vcodec libx264 -vpre hq -vpre ipod320 -b 700k -bt 700k -metadata "title=%TITLE%" -threads 0 -f ipod "%INPUT%.mp4"
+
+endlocal
